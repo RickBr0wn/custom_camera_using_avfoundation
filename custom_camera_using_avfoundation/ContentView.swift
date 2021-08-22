6040a1d96623b4637b8d008aa821e7931989fcf0
Rick Brown <ricky.brown.00@gmail.com>
2021-08-22 13:10:16 +0100

    Initial Commit

---
+++ custom_camera_using_avfoundation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  custom_camera_using_avfoundation
+//
+//  Created by Rick Brown on 22/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ custom_camera_using_avfoundation/custom_camera_using_avfoundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  custom_camera_using_avfoundationApp.swift
+//  custom_camera_using_avfoundation
+//
+//  Created by Rick Brown on 22/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct custom_camera_using_avfoundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
